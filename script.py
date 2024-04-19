bbfc9b9fd074d4e15f18a8aadde99735f5f317e3
andrei <112718960+jekeim@users.noreply.github.com>
2024-04-19 23:29:24 +0300

    commit1

---
+++ script.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import re
+
+def extract_words_from_link(url):
+    
+    pattern = r'\b\w+\b'
+    words = re.findall(pattern, url)
+    return words
+
+def parse(csv_file, n):
+    df = pd.read_csv(csv_file)
+    
+    for link in df.iloc[0:n, 0]:
+        print(extract_words_from_link(str(link)))
+
+if __name__ == '__main__':
+    csv_file = './url_product_extraction_input_dataset.csv'  # Replace with your CSV file path
+    
+    parse(csv_file, 10)
