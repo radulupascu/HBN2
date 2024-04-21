b86550d9c1669f498f87b5edc33c51b0034231ea
andrei <112718960+jekeim@users.noreply.github.com>
2024-04-21 04:41:10 +0300

    mesaj

--- get_product_names.py
+++ get_product_names.py
@@ -1,6 +1,7 @@
 import ollama
 import pandas as pd
 import multiprocessing
+import time
 
 pre_prompt = "Please analyze the provided URL and predict the product or service name it leads to. If you cannot identify the product or service, please respond with \"None\". I need you to respond only with your best guess, or just say \"None\" if you can't decide. Please do not reply with any additional words. Try to match your responses as closely as possible to the words in the URL."
 
@@ -29,25 +30,25 @@ def get_product_names(start_index, finish_index):
   lista = []
 
   for link in df.iloc[start_index:finish_index, 0]:
-      user_input = link
+    user_input = link
 
-      messages.append(
-          {
-              'role': 'user',
-              'content': user_input,
-          },
-      )
+    messages.append(
+        {
+            'role': 'user',
+            'content': user_input,
+        },
+    )
 
-      response = ollama.chat(model="llama3", messages=messages)
+    response = ollama.chat(model="llama3", messages=messages)
 
-      messages.append(
-          {
-              'role': 'assistant',
-              'content': response['message']['content'],
-          },
-      )
-      lista.append(response['message']['content'])
-    
+    messages.append(
+        {
+            'role': 'assistant',
+            'content': response['message']['content'],
+        },
+    )
+    lista.append(response['message']['content'] + ",")
+  print(lista)
   csv_file = pd.DataFrame(columns=["product_name"])
   new_df = pd.DataFrame(lista, columns=csv_file.columns)
   csv_file = pd.concat([csv_file, new_df], ignore_index=True)
@@ -56,11 +57,19 @@ def get_product_names(start_index, finish_index):
    #print(response['message']['content'])
 
 if __name__ == "__main__":
-  # noWorkers = multiprocessing.cpu_count()
-  # pool = multiprocessing.Pool(noWorkers)
-  # for i in range(0, 1000, 20):
-  #     pool.apply_async(func=get_product_names, args=(i, i+20))
 
-  # pool.close()
-  # pool.join()
-  get_product_names(20, 50)
+  start_time = time.time()
+
+  noWorkers = multiprocessing.cpu_count()
+  pool = multiprocessing.Pool(noWorkers)
+  for i in range(0, 10, 20):
+      pool.apply_async(func=get_product_names, args=(i, i+20))
+
+pool.close()
+pool.join()
+#get_product_names(0, 10)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Timp petrecut: {elapsed_time} seconds")
+
+  
