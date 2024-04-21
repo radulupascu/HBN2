a52ac10c59d52ab288e46bfd72d0ec3565b8e4f9
andrei <112718960+jekeim@users.noreply.github.com>
2024-04-21 06:36:22 +0300

    merge

--- get_product_names.py
+++ get_product_names.py
@@ -3,14 +3,14 @@ import pandas as pd
 import multiprocessing
 import time
 
-pre_prompt = "Please analyze the provided URL and predict the product or service name it leads to. If you cannot identify the product or service, please respond with \"None\". I need you to respond only with your best guess, or just say \"None\" if you can't decide. Please do not reply with any additional words. Try to match your responses as closely as possible to the words in the URL."
+pre_prompt = "Please analyze the provided URL and predict the product or service name it leads to. If you cannot identify the product or service, please respond with \"None\". I need you to respond only with your best guess, or just say \"None\" if you can't decide. Please do not reply with any additional words. Try to match your responses as closely as possible to the words in the URL. For each URL, I need your response on a newline. Do not echo the input you get."
 
 modelfile = """
 FROM llama3
 PARAMETER temperature 0.08
-PARAMETER num_ctx 2048
+PARAMETER num_ctx 1024
 PARAMETER repeat_last_n 0
-PARAMETER tfs_z 2.0
+PARAMETER seed 1
 SYSTEM {pre_prompt}
 """.format(pre_prompt=pre_prompt)
 
@@ -23,38 +23,49 @@ messages = [
   },
 ]
 
-def get_product_names(start_index, finish_index):
+
+def get_product_names(start_index):
+
+  batch_size = 20
+
   df = pd.read_csv('./url_product_extraction_input_dataset.csv')
       
-  nr = 0
   lista = []
-
-  for link in df.iloc[start_index:finish_index, 0]:
-    user_input = link
-
-    messages.append(
-        {
-            'role': 'user',
-            'content': user_input,
-        },
-    )
-
-    response = ollama.chat(model="llama3", messages=messages)
-
-    messages.append(
-        {
-            'role': 'assistant',
-            'content': response['message']['content'],
-        },
-    )
-    lista.append(response['message']['content'] + ",")
+  for link in df.iloc[start_index:start_index + batch_size, 0]:
+    lista.append(link)
   print(lista)
+      
+  user_input = '\n'.join(lista)
+
+  messages.append(
+      {
+          'role': 'user',
+          'content': user_input,
+      },
+  )
+
+  response = ollama.chat(model="llama3", messages=messages)
+
+  messages.append(
+      {
+          'role': 'assistant',
+          'content': response['message']['content'],
+      },
+  )
+  #lista.append(response['message']['content'] + ",")
+
+  lista_csv = []
+
+  print(response['message']['content'] + ",")
+
+  for i in range(0, len(lista)):
+    lista_csv.append(response['message']['content'].split('\n')[i] + ",")
+
   csv_file = pd.DataFrame(columns=["product_name"])
-  new_df = pd.DataFrame(lista, columns=csv_file.columns)
+  new_df = pd.DataFrame(lista_csv, columns=csv_file.columns)
   csv_file = pd.concat([csv_file, new_df], ignore_index=True)
   csv_file.to_csv("./product_names.csv", mode='a', header=False, index=False)
 
-   #print(response['message']['content'])
 
 if __name__ == "__main__":
 
@@ -62,14 +73,14 @@ if __name__ == "__main__":
 
   noWorkers = multiprocessing.cpu_count()
   pool = multiprocessing.Pool(noWorkers)
-  for i in range(0, 10, 20):
-      pool.apply_async(func=get_product_names, args=(i, i+20))
+  for i in range(0, 1000, 20):
+    pool.apply_async(func=get_product_names, args=(i, i+20))
 
-pool.close()
-pool.join()
-#get_product_names(0, 10)
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"Timp petrecut: {elapsed_time} seconds")
+  pool.close()
+  pool.join()
+  #get_product_names(0)
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  print(f"Timp petrecut: {elapsed_time} seconds")
 
   
